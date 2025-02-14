e462eaa55cfa4dc61534e61fc43a7bcbe6ceeaf3
veeresh <veereshgujjula254@gmail.com>
2025-02-14 14:28:39 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,14 +1,66 @@
 #include<stdio.h>
+#define MAX 4
+int stack[MAX];
+int top=0;
+void push()
+{
+	int data;
+	if(MAX==top)
+	{
+		printf("stack overflow.\n");
+	}
+	else
+	{
+		printf("enter the data.\n");
+		scanf("%d",&data);
+		stack[top]=data;
+		top=top+1;
+	
+	}
+}
+void pop()
+{
+	if(top==0)
+	{
+		printf("stack is empty.\n");
+	}
+	else
+	{
+		printf("stack removed data =%d\n",stack[--top]);
+	}
+}
+void display()
+{
+	int i;
+	if(top==0)
+	{
+		printf("stack is empty\n");
+	}
+	else
+	{
+		for(i=0;i<top;i++)
+			printf("%d ",stack[i]);
+	}
+	printf("\n");
+}
 int main()
 {
 	int i,num;
-	printf("1.push\n 2.pop\n 3.display\n");
+	printf(" 1.push\n 2.pop\n 3.display\n");
+	while(1)
+	{
 	printf("enter the number\n");
-	scanf("%d",&number);
+	scanf("%d",&num);
 	switch(num)
 	{
-		case 
+		case 1: push();
+				break;
+		case 2:pop();
+				break;
+		case 3:display();
+				break;
+		default:printf("enter the correct number..\n");
+		//exit (0);
 	}
-	
-	
+	}
 }
