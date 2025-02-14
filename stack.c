4eb1907a8423db31e631017ee72e074ac72ee1a0
veeresh <veereshgujjula254@gmail.com>
2025-02-14 14:25:50 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,num;
+	printf("1.push\n 2.pop\n 3.display\n");
+	printf("enter the number\n");
+	scanf("%d",&number);
+	switch(num)
+	{
+		case 
+	}
+	
+	
+}
